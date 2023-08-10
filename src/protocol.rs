b4751dd07b374dd609a9cde6e99a1991ba1a03b5
sunnysab <i@sunnysab.cn>
2023-08-10 20:18:33 +0800

    完成 daemon 与 client 通信.

---
+++ src/protocol.rs
@@ -0,0 +1,79 @@
+use crate::downloader::DownloadTask;
+use crate::CtFile;
+
+use anyhow::Result;
+use bincode::{Decode, Encode};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpStream;
+
+#[derive(Encode, Decode)]
+pub struct TaskStatus {
+    pub name: String,
+
+    pub received: usize,
+    pub total: usize,
+
+    pub is_finished: bool,
+    pub is_failed: bool,
+    pub fail_message: Option<String>,
+}
+
+impl From<&DownloadTask> for TaskStatus {
+    fn from(task: &DownloadTask) -> Self {
+        TaskStatus {
+            name: task.name.clone(),
+            received: task.progress.received(),
+            total: task.progress.total(),
+            is_finished: task.progress.is_finished(),
+            is_failed: task.progress.is_failed(),
+            fail_message: task.progress.get_err_message(),
+        }
+    }
+}
+
+#[derive(Encode, Decode)]
+pub enum Command {
+    List,
+    Add(CtFile),
+}
+
+#[derive(Encode, Decode)]
+pub enum CommandResult {
+    List(Vec<TaskStatus>),
+    Add,
+}
+
+pub struct BinStream {
+    stream: TcpStream,
+
+    tx_buffer: Vec<u8>,
+    rx_buffer: Vec<u8>,
+}
+
+impl BinStream {
+    pub fn new(stream: TcpStream) -> Self {
+        Self {
+            stream,
+            tx_buffer: Vec::with_capacity(1024),
+            rx_buffer: Vec::with_capacity(1024),
+        }
+    }
+
+    pub async fn send<T: Encode>(&mut self, payload: T) -> Result<()> {
+        let config = bincode::config::standard();
+        bincode::encode_into_slice(payload, &mut self.tx_buffer, config)?;
+
+        self.stream.write_u16(self.tx_buffer.len() as u16).await?;
+        self.stream.write_all(&self.tx_buffer).await?;
+        Ok(())
+    }
+
+    pub async fn recv<T: Decode>(&mut self) -> Result<T> {
+        let len = self.stream.read_u16().await? as usize;
+        self.stream.read(&mut self.rx_buffer[..len]).await?;
+
+        let config = bincode::config::standard();
+        let (result, _) = bincode::decode_from_slice(&self.rx_buffer[..len], config)?;
+        Ok(result)
+    }
+}
